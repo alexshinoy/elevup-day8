240b2c7bd436dfb870a747b8617c13f38a2ec41d
Alex Shinoy <alexshinoy99@gmail.com>
2026-06-17 23:59:45 +0530

    Final mobile sidebar fix

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,6 @@ import { Link, useLocation } from "react-router-dom";
 
 export default function Sidebar({ isOpen }) {
   const location = useLocation();
-
   const getStyle = (path) => ({
     textDecoration: 'none',
     color: 'var(--text-main)',
